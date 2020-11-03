7e93d7ba401b2ed79e40901583970f6f00911a3b
Cocodevlab <49289541+Cocodevlab@users.noreply.github.com>
2020-11-02 23:34:07 -0500

    PS4

---
+++ pSets/PS4/election_forecast/scripts/ipums_cleaning.R
@@ -0,0 +1,77 @@
+#### Preamble ####
+# Purpose: Prepare and clean survey data downloaded from IPUMS
+# Author: Isaac Ehrlich
+# Data: 02 November 2020
+# Contact: isaac.ehrlich@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the IPUMS data and saved it to the data folder
+
+
+#### Workspace Setup ####
+library(haven)
+library(tidyverse)
+library(labelled)
+# Read in raw data
+ipums_raw <- read_dta("data/usa_00001.dta")
+
+# Add Labels
+ipums_raw <- labelled::to_factor(ipums_raw)
+
+# Keep Variables of Interest 
+names(ipums_raw)
+
+ipums_data <- 
+  ipums_raw %>% 
+  select(region,
+         stateicp,
+         density, 
+         metro,
+         sex,
+         age,
+         marst,
+         race, 
+         hispan,
+         bpl,
+         citizen,
+         educd,
+         labforce,
+         inctot)
+rm(ipums_raw)
+         
+
+#### Data Cleaning ####
+# Make age groups, based on generational breaks provided by Gallup
+ipums_data$age <- as.numeric(ipums_data$age)
+ipums_data <- filter(ipums_data, age >= 18)
+ipums_data <- ipums_data %>% mutate(age = case_when(age >= 18  & age < 35 ~ '18-34',
+                                              age >= 35  & age < 52 ~ '35-51',
+                                              age >= 52  & age < 68 ~ '52-67',
+                                              age>=68 ~ '68+'))
+
+# Make birthplace binary (in US/not in US)
+ipums_data <- ipums_data %>% mutate(bpl = case_when(bpl %in% tolower(state.name) ~ 'The United States',
+                                                    !(bpl %in% tolower(state.name)) ~ 'Another country'))
+
+# Combine race to match Democracy Fund Data
+ipums_data <- ipums_data %>% mutate(race = case_when(race == "white" ~ 'White',
+                                                     race == "black/african american/negro" ~ 'Black',
+                                                     race == "american indian or alaska native" ~ 'Other',
+                                                     race == "chinese" ~ 'Asian',
+                                                     race == "japanese" ~ 'Asian',
+                                                     race == "other asian or pacific islander" ~ 'Asian',
+                                                     race == "other race, nec" ~ 'Other',
+                                                     race == "two major races" ~ 'Other',
+                                                     race == "three or more major races" ~ 'Other'))
+
+# Match gender to Democracy Fund Data
+ipums_data <- ipums_data %>% mutate(sex = case_when(sex == "female" ~ 'Female',
+                                                    sex == "male" ~ 'Male'))
+
+# Match income to Democracy Fund Data
+ipums_data <- ipums_data %>% mutate(inctot = case_when(inctot < 50000 ~ 'Less than $50,000',
+                                                       inctot >= 50000  & inctot < 100000 ~ '$50,000 to $99,999',
+                                                       inctot >= 100000  & inctot < 250000 ~ '$100,000 to $249,999',
+                                                       inctot >= 250000 ~ "More than $250,000"))
+         
+write.csv(ipums_data, "data/ipums_data_clean.csv")
---
+++ pSets/PS4/election_forecast/scripts/nationscape_cleaning.R
@@ -0,0 +1,111 @@
+#### Preamble ####
+# Purpose: Prepare and clean survey data downloaded from Democracy Fund
+# Author: Isaac Ehrlich
+# Data: 02 November 2020
+# Contact: isaac.ehrlich@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the Democracy Fund data and saved it to the data folder
+
+
+#### Workspace Setup ####
+library(haven)
+library(tidyverse)
+library(dplyr)
+# Read in raw data
+df_raw <- read_dta("data/ns20200625.dta")
+
+# Add Labels
+df_raw <- labelled::to_factor(df_raw)
+
+# Just keep some variables
+df_data <- 
+  df_raw %>% 
+  select(right_track,
+         economy_better,
+         interest,
+         registration,
+         pres_approval,
+         vote_2016,
+         consider_trump,
+         not_trump,
+         trump_biden,
+         primary_party,
+         vote_intention,
+         vote_2020,
+         ideo5,
+         employment,
+         foreign_born,
+         gender,
+         census_region,
+         hispanic,
+         race_ethnicity,
+         household_income,
+         education,
+         state,
+         age)
+
+rm(df_raw)
+
+
+#### Data Cleaning ####
+
+# Change planned vote to show 0 for Donald Trump and 1 for Joe Biden
+df_data <- df_data %>% mutate(vote_2020 = case_when(vote_2020 == "Donald Trump" ~ 0,
+                                              vote_2020 == "Joe Biden" ~ 1))
+df_data <- df_data[!is.na(df_data$vote_2020),]
+
+
+# Make age groups, based on generational breaks provided by Gallup
+df_data <- df_data %>% mutate(age = case_when(age >= 18  & age < 35 ~ '18-34',
+                                             age >= 35  & age < 52 ~ '35-51',
+                                             age >= 52  & age < 68 ~ '52-67',
+                                             age>=68 ~ '68+'))
+
+# Combine race to match IPUMS Data
+df_data <- df_data %>% mutate(race_ethnicity = case_when(race_ethnicity == "White" ~ 'White',
+                                                     race_ethnicity == "American Indian or Alaska Native" ~ 'Other',  
+                                                     race_ethnicity == "Black, or African American" ~ 'Black',
+                                                     race_ethnicity == "Asian (Asian Indian)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Chinese)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Filipino)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Japanese)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Korean)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Vietnamese)" ~ 'Asian',
+                                                     race_ethnicity == "Asian (Other)" ~ 'Asian',
+                                                     race_ethnicity == "Pacific Islander (Native Hawaiian)" ~ 'Asian',
+                                                     race_ethnicity == "Pacific Islander, (Guamanian)" ~ 'Asian',
+                                                     race_ethnicity == "Pacific Islander (Samoan)" ~ 'Asian',
+                                                     race_ethnicity == "Pacific Islander (Other)" ~ 'Asian',
+                                                     race_ethnicity == "Some other race" ~ 'Other'))
+
+# Combine income classes
+df_data <- df_data %>% mutate(household_income = case_when(household_income == "Less than $14,999" ~ 'Less than $50,000',
+                                                         household_income == "$15,000 to $19,999" ~ 'Less than $50,000',
+                                                         household_income == "$20,000 to $24,999" ~ 'Less than $50,000',
+                                                         household_income == "$25,000 to $29,999" ~ 'Less than $50,000',
+                                                         household_income == "$30,000 to $34,999" ~ 'Less than $50,000',
+                                                         household_income == "$35,000 to $39,999" ~ 'Less than $50,000',
+                                                         household_income == "$40,000 to $44,999" ~ 'Less than $50,000',
+                                                         household_income == "$45,000 to $49,999" ~ 'Less than $50,000',
+                                                         household_income == "$50,000 to $54,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$55,000 to $59,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$60,000 to $64,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$65,000 to $69,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$70,000 to $74,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$75,000 to $79,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$80,000 to $84,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$85,000 to $89,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$90,000 to $94,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$95,000 to $99,999" ~ '$50,000 to $99,999',
+                                                         household_income == "$100,000 to $124,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$125,000 to $149,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$150,000 to $174,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$175,000 to $199,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$200,000 to $224,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$225,000 to $249,999" ~ '$100,000 to $249,999',
+                                                         household_income == "$250,000 and above" ~ 'More than $250,000'))
+
+df_data <- df_data[!is.na(df_data$race_ethnicity),]
+
+write.csv(df_data, "data/df_data_clean.csv")
